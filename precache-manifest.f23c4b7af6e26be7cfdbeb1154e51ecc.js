76ae2370ea746825e6899c6df4c692b518850324
michael <michael.anthony.allan@gmail.com>
2020-05-21 16:49:15 +0100

    Updates

---
+++ precache-manifest.f23c4b7af6e26be7cfdbeb1154e51ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3404dfdc2cab07da96448ba29cd3ffac",
+    "url": "/pokemon_2020_v2/index.html"
+  },
+  {
+    "revision": "8e9a83ddd6bb2e4a830c",
+    "url": "/pokemon_2020_v2/static/css/main.1e8fe0af.chunk.css"
+  },
+  {
+    "revision": "c738fcd2f3e947181c67",
+    "url": "/pokemon_2020_v2/static/js/2.f2288221.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon_2020_v2/static/js/2.f2288221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9a83ddd6bb2e4a830c",
+    "url": "/pokemon_2020_v2/static/js/main.d2dcc8d3.chunk.js"
+  },
+  {
+    "revision": "0e4a30bb7c5204397005",
+    "url": "/pokemon_2020_v2/static/js/runtime-main.6b9dc7a4.js"
+  }
+]);
